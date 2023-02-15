3f640baf197fae3c392c7345faac3b1b06bd8a34
riverkasinskii <kinderplus8@gmail.com>
2023-02-15 13:04:24 +0300

    Implement Bullet added

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,24 +3,49 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 1f;
-    [SerializeField] private GameObject player;
+    private GameObject player;
+    private new Rigidbody2D rigidbody2D;
 
-    private new Rigidbody2D rigidbody2D;    
+    private bool isPlayerState;
 
     private void Start()
     {                
-        rigidbody2D = GetComponent<Rigidbody2D>();        
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        player = GameObject.FindWithTag("Player");
     }        
 
     private void Update()
     {
-        
-        rigidbody2D.velocity = new Vector2(moveSpeed, 0f);        
-        FlipEnemyFacing(); 
+        isPlayerState = player.transform.position.x > transform.position.x;
+
+        MoveEnemy(isPlayerState);
+        FlipEnemyFacing();
+        PlayerTracking();        
+    }
+
+    private void MoveEnemy(bool state)
+    {
+        if (state)
+        {
+            rigidbody2D.velocity = new Vector2(moveSpeed, 0f);
+        }
+        else
+        {
+            rigidbody2D.velocity = new Vector2(-moveSpeed, 0f);
+        }
     }
 
     private void FlipEnemyFacing()
     {
         transform.localScale = new Vector2(Mathf.Sign(rigidbody2D.velocity.x), 1f);
     }
+
+    private void PlayerTracking()
+    {
+        transform.position = Vector2.MoveTowards(transform.position,
+                                                 player.transform.position,
+                                                 moveSpeed * Time.deltaTime);
+    }
+        
+    
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,26 +4,51 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float runSpeed = 10f;
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private Transform gun;
 
     private Vector2 moveInput;
+
     private new Rigidbody2D rigidbody2D;
+    private new Collider2D collider2D;
     private Animator animator;
+        
+    bool isAlive = true;
 
     private void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        collider2D = GetComponent<Collider2D>();
     }
         
     private void Update()
     {
+        if (!isAlive) 
+        { 
+            return; 
+        }
         RunHorizontal();
         RunVertical();
-        FlipSprite();        
+        FlipSprite();
+        Die();
+    }
+
+    private void OnFire(InputValue inputValue)
+    {
+        if (!isAlive)
+        {
+            return;
+        }
+        Instantiate(bullet, gun.position, transform.rotation);
     }
 
     private void OnMove(InputValue inputValue)
     {
+        if (!isAlive)
+        {
+            return;
+        }
         moveInput = inputValue.Get<Vector2>();        
     }
 
@@ -51,5 +76,14 @@ public class Player : MonoBehaviour
         {
             transform.localScale = new Vector2(Mathf.Sign(rigidbody2D.velocity.x), 1f);
         }        
+    }
+
+    private void Die()
+    {
+        if (collider2D.IsTouchingLayers(LayerMask.GetMask("Enemies")))
+        {
+            isAlive = false;
+            animator.SetTrigger("Dying");            
+        }
     }
 }
